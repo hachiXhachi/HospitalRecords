940834da5083f4e00dac2ae49f9d4c7cb284763a
hachiXhachi <hhachiman26@gmail.com>
2025-01-16 13:16:15 +0800

    add validation

--- PatientRecord1/Controllers/PatientController.cs
+++ PatientRecord1/Controllers/PatientController.cs
@@ -17,24 +17,39 @@ namespace PatientRecord1.Controllers
         }
         public JsonResult add(Patient pat)
         {
-            HospitalRecordEntities hr = new HospitalRecordEntities();
-            hr.Patients.Add(pat);
-            hr.SaveChanges();
-            return Json(pat);
+            try
+            {
+                HospitalRecordEntities hr = new HospitalRecordEntities();
+                hr.Patients.Add(pat);
+                hr.SaveChanges();
+                return Json(pat);
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = ex.Message });
+            }
         }
         public ActionResult update(Patient pat)
         {
-            HospitalRecordEntities hr = new HospitalRecordEntities();
-            Patient patientUpdate = (from x in hr.Patients where x.ID == pat.ID select x).FirstOrDefault();
-            patientUpdate.Firstname = pat.Firstname;
-            patientUpdate.Middlename = pat.Middlename;
-            patientUpdate.Lastname = pat.Lastname;
-            patientUpdate.Suffixname = pat.Suffixname;
-            patientUpdate.BirthDate = pat.BirthDate;
-            patientUpdate.Gender = pat.Gender;
-            patientUpdate.InitialDiagnosis = pat.InitialDiagnosis;
-            hr.SaveChanges();
-            return Json(new { success = true, message = "Patient updated successfully." });
+            try
+            {
+                HospitalRecordEntities hr = new HospitalRecordEntities();
+                Patient patientUpdate = (from x in hr.Patients where x.ID == pat.ID select x).FirstOrDefault();
+                patientUpdate.Firstname = pat.Firstname;
+                patientUpdate.Middlename = pat.Middlename;
+                patientUpdate.Lastname = pat.Lastname;
+                patientUpdate.Suffixname = pat.Suffixname;
+                patientUpdate.BirthDate = pat.BirthDate;
+                patientUpdate.Gender = pat.Gender;
+                patientUpdate.InitialDiagnosis = pat.InitialDiagnosis;
+                hr.SaveChanges();
+                return Json(new { success = true, message = "Patient updated successfully." });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = ex.Message });
+            }
+        
         }
 
         public ActionResult deletePatient(int id)
